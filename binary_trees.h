b8654eac0e125c9894075278b5bba450fe10e484
Khadaassi <aassikhadija@gmail.com>
2024-01-09 09:40:12 +0000

    Add new binary tree functions

--- binary_trees.h
+++ binary_trees.h
@@ -33,7 +33,16 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-
+int binary_tree_height(const binary_tree_t *tree);
+int binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t $binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /* Print functions*/
 void binary_tree_print(const binary_tree_t *tree);
 
