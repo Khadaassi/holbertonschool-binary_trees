40b7bd4cd6abb09bcc811dd1da4b80d0d3907e72
Khadaassi <aassikhadija@gmail.com>
2024-01-10 14:11:20 +0100

    Refactor binary_tree_inorder function

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,11 +11,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	if (tree && func)
 	{
-	/* first recur on left child */
 		binary_tree_inorder(tree->left, func);
-	/* then print the data of node */
 		func(tree->n);
-	/* now recur on right child */
 		binary_tree_inorder(tree->right, func);
 	}
 }
