2295b85f433bd623d734a699eed4d6d5e91c4c92
Khadaassi <aassikhadija@gmail.com>
2024-01-10 14:18:11 +0100

    Refactor binary_tree_nodes function to handle edge cases and improve readability

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,13 +6,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    if (tree == NULL)
-        return (0);
-    if (tree->left != NULL || tree->right != NULL)
-        nodes++;
-    nodes += binary_tree_nodes(tree->left);
-    nodes += binary_tree_nodes(tree->right);
-    return (nodes);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+	return (nodes + 1);
 }
