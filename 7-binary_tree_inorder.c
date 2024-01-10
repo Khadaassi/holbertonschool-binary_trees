82dbcabdab09774c365dc87092a298cd43c1e2ac
Khadaassi <aassikhadija@gmail.com>
2024-01-10 14:12:55 +0100

    Remove NULL check for tree and func in binary_tree_inorder

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -7,8 +7,6 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-		return;
 	if (tree && func)
 	{
 		binary_tree_inorder(tree->left, func);
